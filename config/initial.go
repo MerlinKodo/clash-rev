b20c625fb155db48d28052ae32e8767fec57dd83
MerlinKodo <150133030+MerlinKodo@users.noreply.github.com>
2023-11-08 05:08:17 -0800

    Add MMDB download functionality and initial config file

--- config/initial.go
+++ config/initial.go
@@ -5,27 +5,50 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/MerlinKodo/clash-rev/component/mmdb"
 	C "github.com/MerlinKodo/clash-rev/constant"
 	"github.com/MerlinKodo/clash-rev/log"
 )
 
+const (
+	mmdbURL           = "https://cdn.jsdelivr.net/gh/MerlinKodo/maxmind-geoip@release/Country.mmdb"
+	initialConfig     = "mixed-port: 7890"
+	directoryPerm     = 0o755
+	filePerm          = 0o644
+	httpClientTimeout = 60 * time.Second
+)
+
+var httpClient = &http.Client{Timeout: httpClientTimeout}
+
 func downloadMMDB(path string) (err error) {
-	resp, err := http.Get("https://cdn.jsdelivr.net/gh/MerlinKodo/maxmind-geoip@release/Country.mmdb")
+	resp, err := httpClient.Get(mmdbURL)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to get MMDB: %w", err)
 	}
 	defer resp.Body.Close()
 
-	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0o644)
-	if err != nil {
-		return err
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("received non-OK HTTP status code: %d", resp.StatusCode)
 	}
-	defer f.Close()
-	_, err = io.Copy(f, resp.Body)
 
-	return err
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, filePerm)
+	if err != nil {
+		return fmt.Errorf("error while opening the file: %w", err)
+	}
+
+	defer func() {
+		if cerr := f.Close(); cerr != nil && err == nil {
+			err = fmt.Errorf("error while closing the file: %w", cerr)
+		}
+	}()
+
+	if _, err := io.Copy(f, resp.Body); err != nil {
+		return fmt.Errorf("failed to write MMDB file: %w", err)
+	}
+
+	return nil
 }
 
 func initMMDB() error {
@@ -39,12 +62,12 @@ func initMMDB() error {
 	}
 
 	if _, err := os.Stat(mmdbPath); os.IsNotExist(err) {
-		log.Infoln("Can't find MMDB, start download")
+		log.Infoln("MMDB not found, starting download")
 		return attemptDownload()
 	} else if !mmdb.Verify() {
-		log.Warnln("MMDB invalid, remove and download")
+		log.Warnln("MMDB is invalid, removing and re-downloading")
 		if err := os.Remove(mmdbPath); err != nil {
-			return fmt.Errorf("can't remove invalid MMDB: %w", err)
+			return fmt.Errorf("failed to remove invalid MMDB: %w", err)
 		}
 		return attemptDownload()
 	}
@@ -56,25 +79,25 @@ func initMMDB() error {
 func Init(dir string) error {
 	// initial homedir
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		if err := os.MkdirAll(dir, 0o777); err != nil {
-			return fmt.Errorf("can't create config directory %s: %s", dir, err.Error())
+		if err := os.MkdirAll(dir, directoryPerm); err != nil {
+			return fmt.Errorf("failed to create config directory %s: %w", dir, err)
 		}
 	}
 
 	// initial config.yaml
 	if _, err := os.Stat(C.Path.Config()); os.IsNotExist(err) {
 		log.Infoln("Can't find config, create a initial config file")
-		f, err := os.OpenFile(C.Path.Config(), os.O_CREATE|os.O_WRONLY, 0o644)
+		f, err := os.OpenFile(C.Path.Config(), os.O_CREATE|os.O_WRONLY, filePerm)
 		if err != nil {
-			return fmt.Errorf("can't create file %s: %s", C.Path.Config(), err.Error())
+			return fmt.Errorf("failed to create config directory %s: %w", C.Path.Config(), err)
 		}
-		f.Write([]byte(`mixed-port: 7890`))
+		f.Write([]byte(initialConfig))
 		f.Close()
 	}
 
 	// initial mmdb
 	if err := initMMDB(); err != nil {
-		return fmt.Errorf("can't initial MMDB: %w", err)
+		return fmt.Errorf("failed to initialize MMDB: %w", err)
 	}
 	return nil
 }
